ac49be1df2921e7ca7a63fc8f858c717a0639401
Daniel Cardenas <danielcar@google.com>
2017-03-09 13:25:16 -0800

    Create new Audio include for TVs that doesn't have ringtones.
    Copied AllAudio.mk, pseudo sorted the list and removed ringtones.
    
    Bug: 36066803
    Test: Build on Fugu
    Change-Id: I2ded2432fd8dd7e15c1b813be9b9fde0c43605c1

--- products/atv_base.mk
+++ products/atv_base.mk
@@ -114,7 +114,7 @@ endif
 # To enable access to /dev/dvb*
 BOARD_SEPOLICY_DIRS += device/google/atv/sepolicy
 
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioTv.mk)
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
 $(call inherit-product-if-exists, frameworks/base/data/fonts/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/dancing-script/fonts.mk)
