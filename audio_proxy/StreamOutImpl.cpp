04167a70d1659ffb11e0f482e85185baf37984f6
yucliu <yucliu@google.com>
2020-09-28 16:23:44 -0700

    [AudioProxy] Client version 5.1
    Upgrade client version to 5.1:
    1. Bump AUDIO_PROXY_MINOR_VERSION, 0 -> 1.
    2. Call IStreamEventListener::onEvent(CLOSE) when audioserver closes
    IStreamOut.
    3. Remove 5.0 impl.
    
    Bug: 167605686
    Test: Manual
    Change-Id: I5fb63ea865114615aa1e8c741df66e037f4861bd

--- audio_proxy/StreamOutImpl.cpp
+++ audio_proxy/StreamOutImpl.cpp
@@ -179,6 +179,12 @@ StreamOutImpl::~StreamOutImpl() {
   mEventFlag.reset();
 }
 
+Return<void> StreamOutImpl::setEventListener(
+    const sp<IStreamEventListener>& listener) {
+  mEventListener = listener;
+  return Void();
+}
+
 Return<uint64_t> StreamOutImpl::getFrameSize() {
   audio_format_t format = static_cast<audio_format_t>(mStream->getFormat());
 
@@ -295,6 +301,11 @@ Result StreamOutImpl::closeImpl() {
   if (mEventFlag) {
     mEventFlag->wake(static_cast<uint32_t>(MessageQueueFlagBits::NOT_EMPTY));
   }
+
+  if (mEventListener) {
+    mEventListener->onClose();
+  }
+
   return Result::OK;
 }
 
