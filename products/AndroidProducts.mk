ea82b38ab53ad217de99ce237afde1108185fef0
Nolen Johnson <johnsonnolen@gmail.com>
2021-01-19 17:39:02 -0500

    products: Add generic x86_64 TV target
    Change-Id: I54362889e840b90c39345ebd5ec55457ca096d42

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -34,6 +34,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_tv_arm64.mk \
     $(LOCAL_DIR)/aosp_tv_x86.mk \
+    $(LOCAL_DIR)/aosp_tv_x86_64.mk \
     $(LOCAL_DIR)/gsi_tv_arm.mk \
     $(LOCAL_DIR)/gsi_tv_arm64.mk \
     $(LOCAL_DIR)/sdk_atv_x86.mk \
