386ce18ab40a27a74a9fd9651c7db59c12938a62
Aaron Kling <webgeek1234@gmail.com>
2021-03-26 18:08:57 -0500

    Remove minimize debug symbol flags
    The flags are set in vendor/lineage, and due to how inherit-product
    works, these flags end up with appended values such as
    'false false', causing the checks to fail.
    
    Change-Id: If323349f2b5b6ed9a137612d643c634b76f4cc03

--- products/atv_lowram_defaults.mk
+++ products/atv_lowram_defaults.mk
@@ -36,14 +36,6 @@ PRODUCT_COPY_FILES += device/google/atv/products/lowram_boot_profiles/preloaded-
 # .art image, and how the corresponding DEX files are laid out.
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := device/google/atv/products/lowram_boot_profiles/boot-image-profile.txt
 
-# Do not generate libartd.
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-
-# Strip the local variable table and the local variable type table to reduce
-# the size of the system image. This has no bearing on stack traces, but will
-# leave less information available via JDWP.
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-
 # Add the system properties.
 TARGET_SYSTEM_PROP += \
     build/make/target/board/go_defaults_common.prop
