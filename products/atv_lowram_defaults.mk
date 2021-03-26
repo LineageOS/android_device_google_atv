28342aa3c2e82e2de5888ee7bc22cac33f498091
Aaron Kling <webgeek1234@gmail.com>
2021-03-26 18:08:57 -0500

    Remove minimize debug symbol flags
    The flags are set in vendor/lineage, and due to how inherit-product
    works, these flags end up with appended values such as
    'false false', causing the checks to fail.
    
    Change-Id: If323349f2b5b6ed9a137612d643c634b76f4cc03

--- products/atv_lowram_defaults.mk
+++ products/atv_lowram_defaults.mk
@@ -31,14 +31,6 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
-# Do not generate libartd.
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-
-# Strip the local variable table and the local variable type table to reduce
-# the size of the system image. This has no bearing on stack traces, but will
-# leave less information available via JDWP.
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-
 # Disable Scudo outside of eng builds to save RAM.
 ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
   PRODUCT_DISABLE_SCUDO := true
