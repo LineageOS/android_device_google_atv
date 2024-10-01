4eaded860d12134ea372e9647067a3c799840836
yucliu <yucliu@google.com>
2024-10-01 15:27:26 -0700

    Remove unused audioproxy configs
    Flag: NONE remove unused code
    Bug: 369429638
    Test: Build
    Change-Id: Ia282b704211dde929e015cda0508c1f6cb3bcb49

--- products/atv_product.mk
+++ products/atv_product.mk
@@ -17,8 +17,6 @@
 # a generic TV device.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 
-PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/atv/audio_proxy/sepolicy/public
-
 PRODUCT_PACKAGES += \
     TvNetworkStackOverlay \
     TvFrameworkOverlay \
