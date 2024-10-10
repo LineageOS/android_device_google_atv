b4d028a3eb77c045ba39f8640030cad59eab505f
Hongguang Chen <hgchen@google.com>
2024-10-10 16:57:18 -0700

    Fix persist log makefile
    Bug: 302609484
    Flag: EXEMPT bug fix
    Test: make and check the properties are in product partition.
    Change-Id: I763491a97236554b55fd2885f5d03ce6c1725296

--- products/atv_logpersist.mk
+++ products/atv_logpersist.mk
@@ -2,7 +2,7 @@
 # See go/agw/platform/system/logging/+/refs/heads/master/logd/README.property for available options
 
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
-PRODUCT_PRODUCT_PROPERTIES +=
+PRODUCT_PRODUCT_PROPERTIES += \
     logd.logpersistd=logcatd \
     logd.logpersistd.size=30 \
     logd.logpersistd.rotate_kbytes=2048
