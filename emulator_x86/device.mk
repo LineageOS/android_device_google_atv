648e66ef06a8e6cef8b527e350f65d56ff267fb7
Roman Kiryanov <rkir@google.com>
2025-05-19 15:39:22 +0000

    Remove a reference to device/generic/goldfish-opengl
    it will be retired
    
    Bug: 416327221
    Test: presubmit
    Change-Id: Icca52203eac379c111ee1426890e02c1942faaa3
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Flag: EXEMPT cleanup

--- emulator_x86/device.mk
+++ emulator_x86/device.mk
@@ -15,7 +15,6 @@
 #
 
 PRODUCT_SOONG_NAMESPACES += device/generic/goldfish # for libwifi-hal-emu
-PRODUCT_SOONG_NAMESPACES += device/generic/goldfish-opengl # for goldfish deps.
 
 ifdef NET_ETH0_STARTONBOOT
   PRODUCT_VENDOR_PROPERTIES += net.eth0.startonboot=1
