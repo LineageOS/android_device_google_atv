34485e8977390fbeeced316381be58445f1eeb0c
Jared Duke <jdduke@google.com>
2026-01-27 16:51:18 -0800

    Remove unused dalvik.vm.madvise.vdexfile.size
    This sysprop has no effect with the launch of
    android.os.disable_madvise_artfile_default.
    
    Bug: 423959896
    Test: presubmit
    Flag: EXEMPT FLAG_REMOVAL
    Change-Id: Id57ca0b936eeadd1ffd7e92fc20d70ed39468299

--- products/atv_lowram_defaults.mk
+++ products/atv_lowram_defaults.mk
@@ -56,16 +56,14 @@ ifeq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
   MALLOC_SVELTE := true
 endif
 
-# Enable Madvising of the art, odex and vdex files to MADV_WILLNEED.
+# Enable Madvising of the odex and vdex files to MADV_WILLNEED.
 # The size specified here is the size limit of how much of the file
 # (in bytes) is madvised.
-# We madvise 0MB of .art file.
 # For odex and vdex files, we limit madvising to 30MB (down from the default of
 # 100MB) to alleviate pagecache pressure.
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.madvise.vdexfile.size=31457280\
-    dalvik.vm.madvise.odexfile.size=31457280\
-    dalvik.vm.madvise.artfile.size=0
+    dalvik.vm.madvise.vdexfile.size=31457280 \
+    dalvik.vm.madvise.odexfile.size=31457280
 
 # Overlay for lowram
 PRODUCT_PACKAGES += TvLowRamOverlay
